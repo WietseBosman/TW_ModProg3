85358053c89dd09c4a6af61f4c7dea63b085f2e6
Wietse <w.m.bosman@students.uu.nl>
2018-12-15 21:15:09 +0100

    First commit with first steps towards the rudementary structure of the project.

---
+++ WindowsFormsApp1/WindowsFormsApp1/ReversiForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class ReversiGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxHelp = new System.Windows.Forms.CheckBox();
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.textBoxSizeX = new System.Windows.Forms.TextBox();
+            this.textBoxSizeY = new System.Windows.Forms.TextBox();
+            this.labelSizeX = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBoxHelp
+            // 
+            this.checkBoxHelp.AutoSize = true;
+            this.checkBoxHelp.Location = new System.Drawing.Point(388, 44);
+            this.checkBoxHelp.Name = "checkBoxHelp";
+            this.checkBoxHelp.Size = new System.Drawing.Size(117, 17);
+            this.checkBoxHelp.TabIndex = 0;
+            this.checkBoxHelp.Text = "Show Legal Moves";
+            this.checkBoxHelp.UseVisualStyleBackColor = true;
+            this.checkBoxHelp.CheckedChanged += new System.EventHandler(this.checkBoxHelp_CheckedChanged);
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.Location = new System.Drawing.Point(291, 40);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonNewGame.TabIndex = 1;
+            this.buttonNewGame.Text = "New Game";
+            this.buttonNewGame.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSizeX
+            // 
+            this.textBoxSizeX.Location = new System.Drawing.Point(154, 28);
+            this.textBoxSizeX.Name = "textBoxSizeX";
+            this.textBoxSizeX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSizeX.TabIndex = 2;
+            // 
+            // textBoxSizeY
+            // 
+            this.textBoxSizeY.Location = new System.Drawing.Point(154, 54);
+            this.textBoxSizeY.Name = "textBoxSizeY";
+            this.textBoxSizeY.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSizeY.TabIndex = 3;
+            // 
+            // labelSizeX
+            // 
+            this.labelSizeX.AutoSize = true;
+            this.labelSizeX.Location = new System.Drawing.Point(79, 31);
+            this.labelSizeX.Name = "labelSizeX";
+            this.labelSizeX.Size = new System.Drawing.Size(69, 13);
+            this.labelSizeX.TabIndex = 4;
+            this.labelSizeX.Text = "Board Width:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Board Height:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelSizeX);
+            this.Controls.Add(this.textBoxSizeY);
+            this.Controls.Add(this.textBoxSizeX);
+            this.Controls.Add(this.buttonNewGame);
+            this.Controls.Add(this.checkBoxHelp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxHelp;
+        private System.Windows.Forms.Button buttonNewGame;
+        private System.Windows.Forms.TextBox textBoxSizeX;
+        private System.Windows.Forms.TextBox textBoxSizeY;
+        private System.Windows.Forms.Label labelSizeX;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/ReversiForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class ReversiGame : Form
+    {
+
+
+
+        public ReversiGame()
+        {
+            InitializeComponent();
+
+            this.Paint += draw;
+        }
+
+        //responsible for drawing the board, score and gamestate.
+        private void draw(object obj, PaintEventArgs pea)
+        {
+
+        }
+
+        //draws the board, optionally including help indications
+        private void drawBoard()
+        {
+
+        }
+
+        //redraws the board when the help checkbox is changed 
+        private void checkBoxHelp_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        //resets the board- and gamestate and resizes the board
+        private void newGame()
+        {
+
+        }
+    }
+
+ }
